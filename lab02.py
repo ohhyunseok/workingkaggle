1460e3e6822957ffda2aae89cc6345446f8d9d62
unknown <Administrator@RE-NOTE02.drbnw.mst>
2020-06-02 17:21:41 +0900

    dev0602

--- lab02.py
+++ lab02.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pandas as pd
 
-train = pd.read_csv("../train.csv")
-test = pd.read_csv("../test.csv")
+train = pd.read_csv("./titanic/train.csv")
+test = pd.read_csv("./titanic/test.csv")
 
 
-# Embarked
+### Embarked
 
 train['Embarked'].isnull().sum()
 # 2
@@ -32,9 +32,73 @@ train['Embarked'].fillna('S', inplace=True)
 # Q     77
 
 
-train['Title'] = train['Name'].str.extract(
-    '([A-Za-z]+)\.', expand=False)
+###Title
 
-# print(train.head(5))
+train['Title'] = train['Name'].str.extract('([A-Za-z]+)\.', expand=False)
+test['Title'] = test['Name'].str.extract('([A-Za-z]+)\.', expand=False)
 
-# print(pd.crosstab(train_data['Title'], train_data['Sex']))
+train['Title'] = train['Title'].replace(['Lady', 'Countess', 'Capt', 'Col', 'Don', 'Dr', 'Majer', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Other')
+train['Title'] = train['Title'].replace('Mlle', 'Miss')
+train['Title'] = train['Title'].replace('Ms', 'Miss')
+train['Title'] = train['Title'].replace('Mme', 'Mrs')
+
+
+test['Title'] = test['Title'].replace(['Lady', 'Countess','Capt', 'Col','Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Other')
+test['Title'] = test['Title'].replace('Mlle', 'Miss')
+test['Title'] = test['Title'].replace('Ms', 'Miss')
+test['Title'] = test['Title'].replace('Mme', 'Mrs')
+
+#print(train['Title'].value_counts())
+
+
+train['Family'] = 1 + train['SibSp'] + train['Parch']
+test['Family'] = 1 + test['SibSp'] + test['Parch']
+
+train['Solo'] = (train['Family']  == 1)
+test['Solo'] = (test['Family']  == 1)
+
+
+train['FareBin'] = pd.qcut(train['Fare'], 5)
+test['FareBin'] = pd.qcut(test['Fare'], 5)
+
+#print(train['FareBin'].value_counts())
+
+
+
+
+train['Age'].isnull().sum()
+# 177
+
+test['Age'].isnull().sum()
+# 86
+
+train["Age"].fillna(train.groupby("Title")["Age"].transform("median"), inplace=True)
+test["Age"].fillna(test.groupby("Title")["Age"].transform("median"), inplace=True)
+
+train.loc[ train['Age'] <= 10, 'Age_clean'] = 0
+train.loc[(train['Age'] > 10) & (train['Age'] <= 16), 'Age_clean'] = 1
+train.loc[(train['Age'] > 16) & (train['Age'] <= 20), 'Age_clean'] = 2
+train.loc[(train['Age'] > 20) & (train['Age'] <= 26), 'Age_clean'] = 3
+train.loc[(train['Age'] > 26) & (train['Age'] <= 30), 'Age_clean'] = 4
+train.loc[(train['Age'] > 30) & (train['Age'] <= 36), 'Age_clean'] = 5
+train.loc[(train['Age'] > 36) & (train['Age'] <= 40), 'Age_clean'] = 6
+train.loc[(train['Age'] > 40) & (train['Age'] <= 46), 'Age_clean'] = 7
+train.loc[(train['Age'] > 46) & (train['Age'] <= 50), 'Age_clean'] = 8
+train.loc[(train['Age'] > 50) & (train['Age'] <= 60), 'Age_clean'] = 9
+train.loc[ train['Age'] > 60, 'Age_clean'] = 10
+
+test.loc[ test['Age'] <= 10, 'Age_clean'] = 0
+test.loc[(test['Age'] > 10) & (test['Age'] <= 16), 'Age_clean'] = 1
+test.loc[(test['Age'] > 16) & (test['Age'] <= 20), 'Age_clean'] = 2
+test.loc[(test['Age'] > 20) & (test['Age'] <= 26), 'Age_clean'] = 3
+test.loc[(test['Age'] > 26) & (test['Age'] <= 30), 'Age_clean'] = 4
+test.loc[(test['Age'] > 30) & (test['Age'] <= 36), 'Age_clean'] = 5
+test.loc[(test['Age'] > 36) & (test['Age'] <= 40), 'Age_clean'] = 6
+test.loc[(test['Age'] > 40) & (test['Age'] <= 46), 'Age_clean'] = 7
+test.loc[(test['Age'] > 46) & (test['Age'] <= 50), 'Age_clean'] = 8
+test.loc[(test['Age'] > 50) & (test['Age'] <= 60), 'Age_clean'] = 9
+test.loc[ test['Age'] > 60, 'Age_clean'] = 10
+
+
+#train.to_csv('train_fill_age.csv',index=False)
+#test.to_csv('test_fill_age.csv',index=False)
