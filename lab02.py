15fa1a8de7acae41375cb6e72a3378ae7c03ca5e
unknown <Administrator@RE-NOTE02.drbnw.mst>
2020-06-15 17:48:16 +0900

    lab02 modify

--- lab02.py
+++ lab02.py
@@ -1,39 +1,61 @@
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import SMOTE
 
 train = pd.read_csv("./titanic/train.csv")
 test = pd.read_csv("./titanic/test.csv")
+gender_submission = pd.read_csv('./titanic/gender_submission.csv')
+submission = pd.read_csv('./titanic/submission.csv')
 
+### Sex
+train['Sex_clean'] = train['Sex'].astype('category').cat.codes
+test['Sex_clean'] = test['Sex'].astype('category').cat.codes
 
 ### Embarked
-
 train['Embarked'].isnull().sum()
 # 2
-
-train['Embarked'].value_counts()
-# S    644
-# C    168
-# Q     77
-
 
 test['Embarked'].isnull().sum()
 # 0
 
-test['Embarked'].value_counts()
-# S    270
-# C    102
-# Q     46
-
-# null 모두 'S'
-train['Embarked'].fillna('S', inplace=True)
-# train['Embarked'].value_counts()
+train['Embarked'].value_counts()
+# output
 # S    644
 # C    168
 # Q     77
 
+train['Embarked'].fillna('S', inplace=True)
+
+train['Embarked_clean'] = train['Embarked'].astype('category').cat.codes
+test['Embarked_clean'] = test['Embarked'].astype('category').cat.codes
+
+
+###Family
+train['Family'] = 1 + train['SibSp'] + train['Parch']
+test['Family'] = 1 + test['SibSp'] + test['Parch']
+
+
+###Solo
+train['Solo'] = (train['Family'] == 1)
+test['Solo'] = (test['Family'] == 1)
+
+
+###Fare
+train['FareBin'] = pd.qcut(train['Fare'], 5)
+test['FareBin'] = pd.qcut(test['Fare'], 5)
+
+#print(train['FareBin'].value_counts())
+
+train['Fare_clean'] = train['FareBin'].astype('category').cat.codes
+test['Fare_clean'] = test['FareBin'].astype('category').cat.codes
+
+#print(train['Fare_clean'].value_counts())
+
 
 ###Title
-
 train['Title'] = train['Name'].str.extract('([A-Za-z]+)\.', expand=False)
 test['Title'] = test['Name'].str.extract('([A-Za-z]+)\.', expand=False)
 
@@ -42,7 +64,6 @@ train['Title'] = train['Title'].replace('Mlle', 'Miss')
 train['Title'] = train['Title'].replace('Ms', 'Miss')
 train['Title'] = train['Title'].replace('Mme', 'Mrs')
 
-
 test['Title'] = test['Title'].replace(['Lady', 'Countess','Capt', 'Col','Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Other')
 test['Title'] = test['Title'].replace('Mlle', 'Miss')
 test['Title'] = test['Title'].replace('Ms', 'Miss')
@@ -50,22 +71,11 @@ test['Title'] = test['Title'].replace('Mme', 'Mrs')
 
 #print(train['Title'].value_counts())
 
-
-train['Family'] = 1 + train['SibSp'] + train['Parch']
-test['Family'] = 1 + test['SibSp'] + test['Parch']
-
-train['Solo'] = (train['Family']  == 1)
-test['Solo'] = (test['Family']  == 1)
+train['Title_clean'] = train['Title'].astype('category').cat.codes
+test['Title_clean'] = test['Title'].astype('category').cat.codes
 
 
-train['FareBin'] = pd.qcut(train['Fare'], 5)
-test['FareBin'] = pd.qcut(test['Fare'], 5)
-
-#print(train['FareBin'].value_counts())
-
-
-
-
+###Age
 train['Age'].isnull().sum()
 # 177
 
@@ -74,6 +84,8 @@ test['Age'].isnull().sum()
 
 train["Age"].fillna(train.groupby("Title")["Age"].transform("median"), inplace=True)
 test["Age"].fillna(test.groupby("Title")["Age"].transform("median"), inplace=True)
+#train["Age"].fillna(train.groupby("Title")["Age"].transform("mean"), inplace=True)
+#test["Age"].fillna(test.groupby("Title")["Age"].transform("mean"), inplace=True)
 
 train.loc[ train['Age'] <= 10, 'Age_clean'] = 0
 train.loc[(train['Age'] > 10) & (train['Age'] <= 16), 'Age_clean'] = 1
@@ -99,6 +111,87 @@ test.loc[(test['Age'] > 46) & (test['Age'] <= 50), 'Age_clean'] = 8
 test.loc[(test['Age'] > 50) & (test['Age'] <= 60), 'Age_clean'] = 9
 test.loc[ test['Age'] > 60, 'Age_clean'] = 10
 
+
+###Cabin
+#print(train['Cabin'].str[:1].value_counts())
+
+mapping = {
+    'A': 0,
+    'B': 1,
+    'C': 2,
+    'D': 3,
+    'E': 4,
+    'F': 5,
+    'G': 6,
+    'T': 7
+}
+
+train['Cabin_clean'] = train['Cabin'].str[:1]
+train['Cabin_clean'] = train['Cabin_clean'].map(mapping)
+train['Cabin_clean'] = train.groupby('Pclass')['Cabin_clean'].transform('median')
+
+test['Cabin_clean'] = test['Cabin'].str[:1]
+test['Cabin_clean'] = test['Cabin_clean'].map(mapping)
+test['Cabin_clean'] = test.groupby('Pclass')['Cabin_clean'].transform('median')
+
+#print(train['Cabin_clean'].value_counts())
+
+feature = [
+    'Pclass',
+    'SibSp',
+    'Parch',
+    'Sex_clean',
+    'Embarked_clean',
+    'Family',
+    'Solo',
+#    'Title_clean',
+    'Age',
+#    'Cabin_clean',
+    'Fare_clean',    
+]
+
+label = [
+    'Survived',
+]
+
+
+#data = train[feature]
+#target = train[label]
+
+#k_fold = KFold(n_splits=10, shuffle=True, random_state=0)
+
+#clf = RandomForestClassifier(n_estimators=50, max_depth=6, random_state=0)
+#print(cross_val_score(clf, data, target, cv=k_fold, scoring='accuracy', ).mean())
+
+
+x_train = train[feature]
+x_test = test[feature]
+y_train = train[label]
+y_test = submission[label]
+
+#scaler = StandardScaler()
+#scaler.fit(x_train)
+#x_train_scaled = scaler.transform(x_train)
+#x_test_scaled = scaler.transform(x_test)
+
+#print(x_train)
+
+##SMOTE 적용
+#smote = SMOTE(random_state=0)
+#x_train_over, y_train_over = smote.fit_sample(x_train, y_train)
+
+clf = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=0)
+clf.fit(x_train, y_train)
+result = clf.score(x_test, y_test)
+print(result)
+
+predictions = clf.predict(x_test)
+
+output = pd.DataFrame({'PassengerId': test.PassengerId, 'Survived': predictions})
+output.to_csv('my_submission_rf_09.csv', index=False)
+
+#gender_submission['Survived'] = clf.predict(x_test)
+#gender_submission.to_csv('titanic-submission.csv',index=False)
 
 #train.to_csv('train_fill_age.csv',index=False)
 #test.to_csv('test_fill_age.csv',index=False)
